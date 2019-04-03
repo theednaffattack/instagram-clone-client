e90772776a27d7788178c751f907742a7e98a2ec
eddie naff <eddienaff@eddienaff.com>
2019-04-03 10:02:10 -0700

    Initial commit

---
+++ generated/apolloComponents.tsx
@@ -0,0 +1,235 @@
+export type Maybe<T> = T | null;
+
+export interface ChangePasswordInput {
+  password: string;
+
+  token: string;
+}
+
+export interface RegisterInput {
+  password: string;
+
+  firstName: string;
+
+  lastName: string;
+
+  email: string;
+}
+
+export interface ProductInput {
+  name: string;
+}
+
+/** The `Upload` scalar type represents a file upload. */
+export type Upload = any;
+
+// ====================================================
+// Documents
+// ====================================================
+
+export type ConfirmUserVariables = {
+  token: string;
+};
+
+export type ConfirmUserMutation = {
+  __typename?: "Mutation";
+
+  confirmUser: boolean;
+};
+
+export type LoginVariables = {
+  email: string;
+  password: string;
+};
+
+export type LoginMutation = {
+  __typename?: "Mutation";
+
+  login: Maybe<LoginLogin>;
+};
+
+export type LoginLogin = {
+  __typename?: "User";
+
+  id: string;
+
+  firstName: string;
+
+  lastName: string;
+
+  email: string;
+
+  name: string;
+};
+
+export type RegisterVariables = {
+  data: RegisterInput;
+};
+
+export type RegisterMutation = {
+  __typename?: "Mutation";
+
+  register: RegisterRegister;
+};
+
+export type RegisterRegister = {
+  __typename?: "User";
+
+  id: string;
+
+  firstName: string;
+
+  lastName: string;
+
+  email: string;
+
+  name: string;
+};
+
+import * as ReactApollo from "react-apollo";
+import * as React from "react";
+
+import gql from "graphql-tag";
+
+// ====================================================
+// Components
+// ====================================================
+
+export const ConfirmUserDocument = gql`
+  mutation ConfirmUser($token: String!) {
+    confirmUser(token: $token)
+  }
+`;
+export class ConfirmUserComponent extends React.Component<
+  Partial<ReactApollo.MutationProps<ConfirmUserMutation, ConfirmUserVariables>>
+> {
+  render() {
+    return (
+      <ReactApollo.Mutation<ConfirmUserMutation, ConfirmUserVariables>
+        mutation={ConfirmUserDocument}
+        {...(this as any)["props"] as any}
+      />
+    );
+  }
+}
+export type ConfirmUserProps<TChildProps = any> = Partial<
+  ReactApollo.MutateProps<ConfirmUserMutation, ConfirmUserVariables>
+> &
+  TChildProps;
+export type ConfirmUserMutationFn = ReactApollo.MutationFn<
+  ConfirmUserMutation,
+  ConfirmUserVariables
+>;
+export function ConfirmUserHOC<TProps, TChildProps = any>(
+  operationOptions:
+    | ReactApollo.OperationOption<
+        TProps,
+        ConfirmUserMutation,
+        ConfirmUserVariables,
+        ConfirmUserProps<TChildProps>
+      >
+    | undefined
+) {
+  return ReactApollo.graphql<
+    TProps,
+    ConfirmUserMutation,
+    ConfirmUserVariables,
+    ConfirmUserProps<TChildProps>
+  >(ConfirmUserDocument, operationOptions);
+}
+export const LoginDocument = gql`
+  mutation Login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      id
+      firstName
+      lastName
+      email
+      name
+    }
+  }
+`;
+export class LoginComponent extends React.Component<
+  Partial<ReactApollo.MutationProps<LoginMutation, LoginVariables>>
+> {
+  render() {
+    return (
+      <ReactApollo.Mutation<LoginMutation, LoginVariables>
+        mutation={LoginDocument}
+        {...(this as any)["props"] as any}
+      />
+    );
+  }
+}
+export type LoginProps<TChildProps = any> = Partial<
+  ReactApollo.MutateProps<LoginMutation, LoginVariables>
+> &
+  TChildProps;
+export type LoginMutationFn = ReactApollo.MutationFn<
+  LoginMutation,
+  LoginVariables
+>;
+export function LoginHOC<TProps, TChildProps = any>(
+  operationOptions:
+    | ReactApollo.OperationOption<
+        TProps,
+        LoginMutation,
+        LoginVariables,
+        LoginProps<TChildProps>
+      >
+    | undefined
+) {
+  return ReactApollo.graphql<
+    TProps,
+    LoginMutation,
+    LoginVariables,
+    LoginProps<TChildProps>
+  >(LoginDocument, operationOptions);
+}
+export const RegisterDocument = gql`
+  mutation Register($data: RegisterInput!) {
+    register(data: $data) {
+      id
+      firstName
+      lastName
+      email
+      name
+    }
+  }
+`;
+export class RegisterComponent extends React.Component<
+  Partial<ReactApollo.MutationProps<RegisterMutation, RegisterVariables>>
+> {
+  render() {
+    return (
+      <ReactApollo.Mutation<RegisterMutation, RegisterVariables>
+        mutation={RegisterDocument}
+        {...(this as any)["props"] as any}
+      />
+    );
+  }
+}
+export type RegisterProps<TChildProps = any> = Partial<
+  ReactApollo.MutateProps<RegisterMutation, RegisterVariables>
+> &
+  TChildProps;
+export type RegisterMutationFn = ReactApollo.MutationFn<
+  RegisterMutation,
+  RegisterVariables
+>;
+export function RegisterHOC<TProps, TChildProps = any>(
+  operationOptions:
+    | ReactApollo.OperationOption<
+        TProps,
+        RegisterMutation,
+        RegisterVariables,
+        RegisterProps<TChildProps>
+      >
+    | undefined
+) {
+  return ReactApollo.graphql<
+    TProps,
+    RegisterMutation,
+    RegisterVariables,
+    RegisterProps<TChildProps>
+  >(RegisterDocument, operationOptions);
+}
---
+++ pages/confirm.tsx
@@ -0,0 +1,44 @@
+import * as React from "react";
+import {
+  ConfirmUserMutation,
+  ConfirmUserVariables
+} from "../generated/apolloComponents";
+import { confirmUserMutation } from "../graphql/user/mutations/confirmUser";
+import { MyContext } from "../interfaces/MyContext";
+import redirect from "../lib/redirect";
+
+export default class Confirm extends React.PureComponent {
+  static async getInitialProps({
+    query: { token },
+    apolloClient,
+    ...ctx
+  }: MyContext) {
+    if (!token) {
+      return {};
+    }
+
+    const trythis = await apolloClient.mutate<
+      ConfirmUserMutation,
+      ConfirmUserVariables
+    >({
+      mutation: confirmUserMutation,
+      variables: {
+        token: token as string
+      }
+    });
+
+    console.log(trythis);
+
+    if (trythis) {
+      redirect(ctx, "/login");
+    } else {
+      return "soemthing went wrong, confirmation mutation";
+    }
+
+    return {};
+  }
+
+  render() {
+    return "something went wrong";
+  }
+}

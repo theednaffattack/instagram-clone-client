d38372093d90c4415d95e871e45923360fc68c5d
eddie naff <eddienaff@eddienaff.com>
2019-06-26 23:45:15 -0700

    Adopt naming change from schema

--- graphql/user/subscriptions/MyFollowerPosts.ts
+++ graphql/user/subscriptions/MyFollowerPosts.ts
@@ -2,7 +2,7 @@ import { gql } from "apollo-boost";
 
 export const MyFollowerPosts = gql`
   subscription MyFollowerPosts($data: QuickPostSubsInput!) {
-    newPost(data: $data) {
+    followingPosts(data: $data) {
       id
       title
       text
